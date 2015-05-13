5d399ab544508434803b5b85591b7dc7bbed58e9
Ranjeet <ranjeetkumar692@gmail.com>
2015-05-13 18:13:28 +0530

    port changed

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 //var seojs = require('express-seojs');
 //app.use(seojs('14f59vej5dr1mmx95pvzb9qlx'));
 
-app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:3000').set('prerenderToken', '9I6PNQ4jxLzvPISvvUo7'));
+app.use(require('prerender-node').set('prerenderServiceUrl', 'http://localhost:1445').set('prerenderToken', '9I6PNQ4jxLzvPISvvUo7'));
 app.use(express.static(__dirname));
 
 app.set('port', process.env.PORT || 1445);
