6a8cd1eea21c2800b76b18d2c509dfc93cfb83fd
Ranjeet <ranjeetkumar692@gmail.com>
2015-08-05 20:52:33 +0530

    CB CloudTable & CB Column

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
 module.exports = function(grunt) {
-  
+
   grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
 		concat: {
             JS : {
                 // the files to concatenate
-                src: ['src/js/Head.html','src/js/Contents.html','src/js/Intro.html','src/js/CloudApp.html','src/js/CloudObject.html','src/js/Relationships.html','src/js/CloudQuery.html','src/js/CloudNotification.html','src/js/CloudSearch.html','src/js/CloudGeoPoint.html','src/js/CloudUser.html','src/js/CloudRole.html','src/js/CloudFile.html','src/js/ACL.html','src/js/JSON.html','src/js/Contribute.html','src/js/Footer.html'],
+                src: ['src/js/Head.html','src/js/Contents.html','src/js/Intro.html','src/js/CloudApp.html','src/js/CloudObject.html','src/js/Relationships.html','src/js/CloudQuery.html','src/js/CloudNotification.html','src/js/CloudSearch.html','src/js/CloudGeoPoint.html','src/js/CloudUser.html','src/js/CloudRole.html','src/js/CloudFile.html','src/js/Column.html','src/js/CloudTable.html', 'src/js/ACL.html','src/js/JSON.html','src/js/Contribute.html','src/js/Footer.html'],
                 	// the location of the resulting JS file
                 	dest: 'index.html'
                 }
