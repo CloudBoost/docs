5cf7d22aa67c063ac9e3ef21b6335c812135798f
rtbathula <battu.network@gmail.com>
2016-03-11 14:49:34 -0300

    added server file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,12 @@
 module.exports = function(grunt) {
-
+  
   grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-
-		concat: {
-            JS : {
-                // the files to concatenate
-                src: ['src/js/Head.html','src/js/Contents.html','src/js/Intro.html','src/js/CloudApp.html','src/js/CloudObject.html','src/js/Relationships.html','src/js/CloudQuery.html','src/js/CloudNotification.html','src/js/CloudSearch.html','src/js/CloudGeoPoint.html','src/js/CloudUser.html','src/js/CloudRole.html','src/js/CloudFile.html','src/js/Column.html','src/js/CloudTable.html', 'src/js/ACL.html','src/js/JSON.html','src/js/Contribute.html','src/js/Footer.html'],
-                	// the location of the resulting JS file
-                	dest: 'index.html'
-                }
-            },
+		connect: {
+			development: {
+				port: 1446
+			}
+		},
 
 		less: {
 		  development: {
@@ -38,8 +34,9 @@ module.exports = function(grunt) {
 	});
 
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['concat:JS']);
+
+	grunt.loadNpmTasks('grunt-connect');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.registerTask('default', 'connect:development');
 
 };
