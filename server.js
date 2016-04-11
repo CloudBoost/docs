72e9b5e4a80fccee0448ea458cf39f7555684730
rtbathula <battu.network@gmail.com>
2016-04-11 18:56:39 -0300

    fixed swagger link bug

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/javascript.html'));
 });
+app.get('/swagger', function(req, res) {
+    res.redirect('https://swagger.cloudboost.io')
+});
 
 app.set('port', process.env.PORT || 1445);
 var server = app.listen(app.get('port'), function() {
