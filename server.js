bdb6a8e60b12dfc645abf5e5d214cc4d7ccb230d
rtbathula <battu.network@gmail.com>
2016-04-11 17:56:32 -0300

    added new file structure and made flow though grunt

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use(express.static(__dirname));
 
-app.set('port', process.env.PORT || 1445);
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/javascript.html'));
+});
 
+app.set('port', process.env.PORT || 1445);
 var server = app.listen(app.get('port'), function() {
-	console.log("CloudBoost API Reference started on PORT:"+app.get('port'));
+	console.log("API Reference started on PORT:"+app.get('port'));
 });
